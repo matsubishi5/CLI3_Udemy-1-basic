ff1d3019e66f0ef7d538fad46429881c41171527
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-08 21:06:39 +0900

    [learn]custum-directive el

--- src/main.js
+++ src/main.js
@@ -4,12 +4,8 @@ import LikeNumber from './components/LikeNumber.vue';
 
 Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive('border', {
-  bind() {},
-  inserted() {},
-  update() {},
-  componentUpdated() {},
-  unbind() {},
+Vue.directive('border', function(el, binding) {
+  el.style.border = '1px solid red';
 });
 
 new Vue({

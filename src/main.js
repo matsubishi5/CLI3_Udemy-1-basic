e33b8f2767be5a51881d292ed511172590b0e3c5
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-08 21:09:29 +0900

    [learn]custum-directive binding

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber);
 Vue.directive('border', function(el, binding) {
   el.style.border = '1px solid red';
+  el.style.borderWidth = binding.value;
 });
 
 new Vue({

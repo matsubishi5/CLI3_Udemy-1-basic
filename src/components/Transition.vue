358a2bc9ad33dddf29b99fd5904f5b94e4e41eaf
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-06-09 11:05:01 +0900

    [learn]transition fade

--- src/components/Transition.vue
+++ src/components/Transition.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button @click="show = !show">表示切り替え</button>
-    <transition>
+    <transition name="fade">
       <p v-if="show">hello</p>
     </transition>
   </div>
@@ -17,4 +17,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 2s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 2s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,6 @@ Vue.component('LikeNumber', LikeNumber);
 Vue.filter('upperCase', function(value) {
   return value.toUpperCase();
 });
-Vue.mixin({
-  created() {
-    console.log('global mixin');
-  },
-});
 
 new Vue({
   render: (h) => h(App),
